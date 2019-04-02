98d0ca78db14f2230363f0f3b4badc9a620047c4
Ahmed Azam <amdnew1@gmail.com>
2019-04-02 11:03:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
+
+
+
 function dwarfRollCall(dwarves) {
+  var newStr = "";
+  dwarves.forEach(function(name, idx) {
+  	newStr += (idx+1 + ". " + name + " ");
+  });
+  return newStr;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+dwarfRollCall(["Dopey","Grumpy","Bashful","Doc"]);
+
+function summonCaptainPlanet(array) {
+	return array.map(function(ele) {
+		return ele.toUpperCase() + "!";
+	});
 }
+
+summonCaptainPlanet(["earth", "wind", "fire", "water", "heart"]);
+
 
 function longPlaneteerCalls(words) {
+	for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}   
+	return false;
 }
+longPlaneteerCalls(["wind", "fire"]);
+
+
+
+
 
 function findTheCheese (foods) {
+    var cheese = ["camembert", "cheddar", "gouda"];
+    for(var i = 0; i < foods.length; i++) {
+        for(var j = i; j < cheese.length; j++){
+           if(foods[i] === cheese[j]) {
+                return cheese[j];
+            }
+        }
+    }
+    return "no cheese!";
 }
+
+findTheCheese(["banana", "cheddar", "sock", "gouda"]);
+
